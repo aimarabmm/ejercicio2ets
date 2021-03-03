811a28c4151054324d69ef8df75d42c31b4dcf6d
1cfgs <1cfgs@A6PC25>
2021-03-03 13:30:51 +0200

    Paso 3 y 4

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -9,10 +9,34 @@
  * @author 1cfgs
  */
 public class Calculadora {
+
     private Integer operador1;
+
+    private Integer operador2;
+
+    private Integer operador3;
+
+    public Integer getOperado1() {
+        return operador1;
+    }
+
+    public Integer getOperado2() {
+        return operador2;
+    }
+
+    public Integer getOperado3() {
+        return operador3;
+    }
     
-   private Integer operador2;
-   
-   private Integer operador3;
-    
+    public Integer setOperado1() {
+        return operador1;
+    }
+
+    public Integer setOperado2() {
+        return operador2;
+    }
+
+    public Integer setOperado3() {
+        return operador3;
+    }
 }

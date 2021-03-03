eccd568a074de3b825a0a6ab022615a384064a09
1cfgs <1cfgs@A6PC25>
2021-03-03 13:23:01 +0200

    Paso 1 y 2

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 1cfgs
+ */
+public class Calculadora {
+    private Integer operador1;
+    
+   private Integer operador2;
+   
+   private Integer operador3;
+    
+}
